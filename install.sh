2d9f1aae6706d2f439bf89b9d718e055db227bf5
micziz <miczicontent@gmail.com>
2022-05-24 14:48:07 +0200

    Finish!

--- install.sh
+++ install.sh
@@ -1,14 +1,26 @@
-if [ command -v python3 >/dev/null 2>&1 ]; then
-    echo "Python3 is not installed"
-    echo "Python3 is required to run pyshell"
-    exit 1
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [ command -v python3 >/dev/null 2>&1 ]; then
+        echo "Python3 is not installed"
+        echo "Python3 is required to run pyshell"
+        exit 1
+    fi  
+else
+    if [ command -v python >/dev/null 2>&1 ]; then
+        echo "Python is not installed"
+        echo "Python is required to run pyshell"
+        exit 1
+    fi
 fi
+
 if [ command -v curl >/dev/null 2>&1 ]; then
     echo "curl is not installed"
     echo "curl is required to install pyscript run pyshell"
     exit 1
 fi
 
-curl -sl https://raw.githubusercontent.com/miczi/pyshell/master/install.sh | sh
+curl -sl https://raw.githubusercontent.com/micziz/pyshell/main/pyshell.py -o pyshell.py
+
+echo "pyshell is installed"
+echo "run pyshell with python3 pyshell.py"
 
 
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Pyshell
+
+Pyshell is a shell for Python.
+
+## Installation
+
+```bash
+curl -sL https://raw.githubusercontent.com/micziz/pyshell/master/install.sh | sh
+```
+
+(requires python and curl)
+
+## Usage
+
+```bash
+python3 pyshell.py
+```
